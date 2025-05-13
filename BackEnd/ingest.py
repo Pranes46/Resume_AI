9ead400dff1dfb1d45c95067f1957c5923932a8e
Pranes Gopalan Venkatesh <pranesgopalanvenkatesh@Mac.attlocal.net>
2025-05-12 20:57:24 -0500

    Deployment changes

--- BackEnd/ingest.py
+++ BackEnd/ingest.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 
 #to load .env
 load_dotenv()
